e64fb811c1e7e4e02378a5bc12dd5bc7a029a564
afrizal <4friizal@gmail.com>
2024-11-17 13:00:03 +0700

    feat : crete sign-up screen

--- client/app/sign-in.tsx
+++ client/app/sign-in.tsx
@@ -66,7 +66,7 @@ export default function SignIn() {
             color: "#125491",
           }}
         >
-          Sign Up
+          Daftar
         </Link>
       </View>
     </SafeAreaView>
--- client/app/sign-up.tsx
+++ client/app/sign-up.tsx
@@ -1,9 +1,75 @@
-import { SafeAreaView, Text } from "react-native";
+import { icons } from "@/constants";
+
+import { Link, router } from "expo-router";
+
+import { FormField } from "@/components";
+import PrimaryButton from "@/components/PrimaryButton";
+import { Image, SafeAreaView, Text, View } from "react-native";
 
 export default function SignUp() {
   return (
-    <SafeAreaView>
-      <Text>This Sign Up</Text>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        paddingHorizontal: 40,
+        gap: 10,
+        backgroundColor: "white",
+      }}
+    >
+      <Image
+        source={icons.LogoPark}
+        style={{
+          marginHorizontal: "auto",
+          width: 80,
+          height: 80,
+        }}
+      />
+      <Text
+        style={{
+          textAlign: "center",
+          fontSize: 24,
+          fontWeight: 700,
+        }}
+      >
+        Buat akun dahulu
+      </Text>
+
+      <FormField placeHolder="Username" />
+      <FormField placeHolder="Email" />
+      <FormField placeHolder="Password" />
+
+      <PrimaryButton
+        title="Daftar"
+        handlerPress={() => router.push("/sign-up")}
+      />
+
+      <View
+        style={{
+          flexDirection: "row",
+          gap: 5,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 14,
+            fontWeight: 700,
+            color: "#333",
+          }}
+        >
+          Sudah punya akun?
+        </Text>
+        <Link
+          href="/sign-in"
+          style={{
+            fontSize: 14,
+            fontWeight: 700,
+            color: "#125491",
+          }}
+        >
+          Masuk
+        </Link>
+      </View>
     </SafeAreaView>
   );
 }
