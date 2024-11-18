80ad85a37aaddb85c4eddd491c0a6da8825386d5
Afrizal <4friizal@gmail.com>
2024-11-18 11:45:08 +0700

    feat : setup bootstrap 5 and create layout

---
+++ laravel/resources/views/homepage.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app') @section( 'content')
+
+<div>
+    <!-- Let all your things have their places; let each part of your business have its time. - Benjamin Franklin -->
+    <div
+        class="btn-group mx-auto mt-10"
+        role="group"
+        aria-label="Basic mixed styles example"
+    >
+        <button type="button" class="btn btn-danger">Left</button>
+        <button type="button" class="btn btn-warning">Middle</button>
+        <button type="button" class="btn btn-success">Right</button>
+    </div>
+</div>
+
+@endsection
