29093cc933ce77474f0cdbaba97a9ebacc9b814f
iglessias <149502331+iglessias@users.noreply.github.com>
2025-01-06 11:55:49 +0700

    memperbaiki format

--- client/app/sign-in.tsx
+++
@@ -1,9 +0,0 @@
-import { SafeAreaView, Text } from "react-native";
-
-export default function SignIn() {
-  return (
-    <SafeAreaView>
-      <Text>om test lagi yaa</Text>
-    </SafeAreaView>
-  );
-}
